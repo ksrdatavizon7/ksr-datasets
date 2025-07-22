31a435453ae9a050827e4413caa1a216a095fdc2
KSR Datavizon <hackathons@datavizon.com>
2025-07-22 09:06:40 +0530

    Add files via upload

---
+++ article files/employees.sql
@@ -0,0 +1,10 @@
+CREATE TABLE employees (
+  EmployeeID INT,
+  Name TEXT,
+  Department TEXT,
+  Salary INT
+);
+
+INSERT INTO employees (EmployeeID, Name, Department, Salary) VALUES (101, 'Alice', 'HR', 50000);
+INSERT INTO employees (EmployeeID, Name, Department, Salary) VALUES (102, 'Bob', 'IT', 60000);
+INSERT INTO employees (EmployeeID, Name, Department, Salary) VALUES (103, 'Charlie', 'Finance', 55000);
